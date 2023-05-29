bbf99f87d17b09b47492304e7296eff07e221963
asubedi <asubedi@go.olemiss.edu>
2023-05-29 14:14:46 -0500

    added server component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    // pass js objects (tell angular the purpose of this class)
+    selector: 'app-server',   // should be a unique selector
+    templateUrl: './server.component.html'
+
+})
+export class ServerComponent {
+
+}
