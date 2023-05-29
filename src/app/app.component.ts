3d3fe275a4655fb4c6777b24b96cdfa360ae5b0a
asubedi <asubedi@go.olemiss.edu>
2023-05-29 14:47:03 -0500

    component styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] // inline styles can be added here too
 })
 export class AppComponent {
   name = 'Amisha';
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  //templateUrl: './servers.component.html', // either point to .html file or write html within this file
+  // defining html with ts file
+  template: `                               
+      <app-server></app-server>
+      <app-server></app-server>
+    `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
