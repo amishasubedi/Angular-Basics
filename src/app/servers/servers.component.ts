bc2db8d9051a641c4327f2af69695d899faa566d
asubedi <asubedi@go.olemiss.edu>
2023-05-31 20:19:16 -0500

    assignment 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,8 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  isEmpty = true;
   serverCreationStatus = "No server was created";
   serverName = '';
+  userName = '';
+  userNameProperty = 'This is the username property!'
+
+
+
+  ngOnInit() { }
+
+  onAddServer() {
+    this.serverCreationStatus = "Server Created! Server name is: " + this.serverName;
+  }
+
+  onAddUser() {
+    this.serverCreationStatus = "User Created! User name is: " + this.userName;
+    this.userName = '';
+    this.isEmpty = true;
+  }
+
+  // event - user input
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value; // explicit casting
+  }
+
+  onUpdateUserName(event: Event) {
+    this.userName = (<HTMLInputElement>event.target).value;
+    this.isEmpty = false;
+  }
 
 
   constructor() {
@@ -22,16 +49,6 @@ export class ServersComponent implements OnInit {
     }, 2000)
   }
 
-  ngOnInit() { }
-
-  onAddServer() {
-    this.serverCreationStatus = "Server Created";
-  }
-
-  // event - user input
-  onUpdateServerName(event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value; // explicit casting
-  }
 
 
 }
