7da27294af1b49f62621effcff715b92f7bfe89d
asubedi <asubedi@go.olemiss.edu>
2023-05-31 20:53:08 -0500

    added dynamic css class with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,12 @@ import { Component } from "@angular/core";
 @Component({
     // pass js objects (tell angular the purpose of this class)
     selector: 'app-server',   // should be a unique selector
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 
 })
 export class ServerComponent {
